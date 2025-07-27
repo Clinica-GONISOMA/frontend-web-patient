e079b33f59ef4c4b85da2b2573b784b6183058d5
NicolasJorquera <nicolas.jorqueramartinez@gmail.com>
2025-07-27 19:46:01 -0400

    [FIX] param name hidden

--- src/app/components/Branding.tsx
+++ src/app/components/Branding.tsx
@@ -3,22 +3,24 @@ import Link from 'next/link';
 
 interface BrandingProps {
     className?: string;
+    nameHidden?: boolean;
 }
 
 export default function Branding( props: BrandingProps ) {
-    const { className } = props;
+    const { className, nameHidden = false} = props;
     return (
         <Link href="/" className={`${className}`}>
             <div className="flex items-center">
 
                 <div className="text-4xl text-center mt-2">🏥</div>
-                <div className='hidden md:flex flex-col'>
-                    <p>Nombre</p>
-                    <p className="text-3xl">Empresa</p>
-                </div>
-
+                {
+                    !nameHidden && 
+                    <div className='hidden md:flex flex-col'>
+                        <p>Nombre</p>
+                        <p className="text-3xl">Empresa</p>
+                    </div>
+                }
             </div>
         </Link>
     );
-
 }
